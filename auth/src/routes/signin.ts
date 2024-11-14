a6e495247f6a3248dbd92b4eb8b6e805ff0327a7
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2024-11-14 22:31:33 +0200

    Update signin.ts

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -3,7 +3,12 @@ import { body, validationResult } from 'express-validator'
 
 const router = Router();
 
-router.post('/api/users/signin', (_req: Request, res: Response) =>{
+router.post('/api/users/signin',  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().isLength({ min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters')
+
+],
+(_req: Request, res: Response) =>{
     res.send('Hi there!')
 })
 
