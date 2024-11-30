9d271f385c03b0eeb0ae19aa775e28093aaa2b27
Mohamed Shebl <hamdysheblm@gmail.com>
2024-11-30 19:55:35 +0200

    Update: complete signup functionality (create token and save it into session cookie)

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,10 @@
 import { Router, Request, Response } from 'express';
 import { body, validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken';
+
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = Router();
 
@@ -9,17 +12,35 @@ router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters')
 
-], (req: Request, res: Response) =>{
+], async(req: Request, res: Response) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) 
         throw new RequestValidationError(errors.array())
     
     const { email, password } = req.body
 
-    // Create a user
-    
+    const existingUser = await User.findOne({email});
 
-    res.send({})
+    if (existingUser) {
+        console.log('Email in use');
+        throw new BadRequestError('Email in use');
+    }
+
+    const user = User.build({email, password})
+
+    await user.save()
+    user.password = ''
+
+    const userJwt = jwt.sign({
+        id: user._id,
+        email: user.email
+    }, 'asdf')
+
+    req.session = {
+        jwt: userJwt
+    }
+
+    res.status(201).send(user)
 })
 
 export{ router as signupRouter }
