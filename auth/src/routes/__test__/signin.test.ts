ec8ea5c93f225cfb5a265c408e5cdb6801c8e7cc
Mohamed Shebl <hamdysheblm@gmail.com>
2024-12-25 22:33:30 +0200

    Add: test for incorrect password supplied

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -10,3 +10,21 @@ it('fails when a email does not exist is supplied', async () => {
 		})
 		.expect(400);
 });
+
+it('fails when an incorrect password is supplied', async () => {
+	await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'user@test.com',
+			password: 'password1234',
+		})
+		.expect(201);
+
+	await request(app)
+		.post('/api/users/signin')
+		.send({
+			email: 'test@test.com',
+			password: 'test1234',
+		})
+		.expect(400);
+});
