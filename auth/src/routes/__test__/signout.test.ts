2a07e7b7e1f83d52cabf5da14d8ad47f7bb04a88
Mohamed Shebl <hamdysheblm@gmail.com>
2024-12-25 22:41:20 +0200

    Add: signout test and check the cookie to be cleared

---
+++ auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import { app } from './../../app';
+
+it('clear the cookie after signing out', async () => {
+	await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'signout@test.com',
+			password: 'password1234',
+		})
+		.expect(201);
+
+	const response = await request(app).post('/api/users/signout').send({});
+
+	const cookie = response.get('Set-Cookie');
+	if (!cookie) {
+		throw new Error('Expected cookie but got undefined.');
+	}
+
+	expect(cookie[0]).toEqual(
+		'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly'
+	);
+});
