083586e259b66571f9ad40d910591f47d557a5aa
Mohamed Shebl <hamdysheblm@gmail.com>
2024-12-25 22:34:09 +0200

    Add: test for valid credentials with returning cookie

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -28,3 +28,23 @@ it('fails when an incorrect password is supplied', async () => {
 		})
 		.expect(400);
 });
+
+it('response with cookie when send valid credentials', async () => {
+	const creationResponse = await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'user@test.com',
+			password: 'password1234',
+		})
+		.expect(201);
+
+	const response = await request(app)
+		.post('/api/users/signin')
+		.send({
+			email: 'user@test.com',
+			password: 'password1234',
+		})
+		.expect(200);
+
+	expect(response.get('Set-Cookie')).toBeDefined();
+});
