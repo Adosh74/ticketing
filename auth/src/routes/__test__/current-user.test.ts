cf9dfb5f641876387ab1a329fa9c1e3114df503d
Mohamed Shebl <hamdysheblm@gmail.com>
2024-12-26 03:14:24 +0200

    Add: test for if not authenticated user

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -12,3 +12,12 @@ it('responds with details about current user', async () => {
 
 	expect(response.body.currentUser.email).toEqual(signinData.user.email);
 });
+
+it('responds with null if not authenticated user', async () => {
+	const response = await request(app)
+		.get('/api/users/currentUser')
+		.send({})
+		.expect(200);
+
+	expect(response.body.currentUser).toBeNull();
+});
