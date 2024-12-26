85385d4a568243d4a84d011cfd6ce8282256c0de
Mohamed Shebl <hamdysheblm@gmail.com>
2024-12-26 03:07:34 +0200

    Add: global signin handler

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,7 +1,19 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 
 import { app } from '../app';
+
+interface GlobalSigninOutput {
+	cookie: string[];
+	user: {
+		email: string;
+		password: string;
+	};
+}
+declare global {
+	var signin: () => Promise<GlobalSigninOutput>;
+}
 
 let mongo: MongoMemoryServer;
 
@@ -26,3 +38,25 @@ afterAll(async () => {
 	mongo ? await mongo.stop() : null;
 	await mongoose.connection.close();
 });
+
+global.signin = async () => {
+	const userData = {
+		email: 'signin@test.com',
+		password: 'password',
+	};
+	const response = await request(app)
+		.post('/api/users/signup')
+		.send(userData)
+		.expect(201);
+
+	const cookie = response.get('Set-Cookie');
+
+	if (!cookie) {
+		throw new Error('Expected cookie but got undefined.');
+	}
+
+	return {
+		cookie,
+		user: userData,
+	};
+};
