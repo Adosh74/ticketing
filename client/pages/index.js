2a0ddb2650732a8b4d3941751285b19cdf696dca
Mohamed Shebl <hamdysheblm@gmail.com>
2025-02-17 23:18:38 +0200

    Add build client in next api

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,16 @@
-const LandingPage = () => {
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+	console.log('currentUser', currentUser);
 	return <h1>Landing Page Working</h1>;
+};
+
+LandingPage.getInitialProps = async (context) => {
+	const client = buildClient(context);
+
+	const { data } = await client.get('/api/users/currentuser');
+
+	return data;
 };
 
 export default LandingPage;
