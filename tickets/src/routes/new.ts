f1302c775b431e4d14b16d85ad3846528ce821ed
Mohamed Shebl <hamdysheblm@gmail.com>
2025-03-03 23:57:18 +0200

    Update complete create new ticket handler

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,11 +1,31 @@
 import { Router, Request, Response } from 'express';
+import { body } from 'express-validator';
 
-import { requireAuth } from '@mshebltickets/common';
+import { requireAuth, validateRequest } from '@mshebltickets/common';
+import { Ticket } from '../models/ticket';
 
 const router = Router();
 
-router.post('/api/tickets', requireAuth, async (req: Request, res: Response) => {
-	res.sendStatus(200);
-});
+router.post(
+	'/api/tickets',
+	requireAuth,
+	[
+		body('title').notEmpty().isString().withMessage('title is required'),
+		body('price')
+			.notEmpty()
+			.isFloat({ gt: 0 })
+			.withMessage('price must be greater 0'),
+	],
+	validateRequest,
+	async (req: Request, res: Response) => {
+		const { price, title } = req.body;
+
+		const ticket = Ticket.build({ price, title, userId: req.currentUser!.id });
+
+		await ticket.save();
+
+		res.status(201).send(ticket);
+	}
+);
 
 export { router as createTicketRouter };
