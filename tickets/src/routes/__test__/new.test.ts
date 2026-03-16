d85e90fbaba7898f3a6b5490fe1430d86be91733
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2026-03-16 23:34:08 +0200

    test: add event publishing test for ticket creation using natsWrapper

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { expect, it } from '@jest/globals';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
 	const response = await request(app).post('/api/tickets').send({});
@@ -80,4 +81,18 @@ it('create a ticket with a valid inputs', async () => {
 	expect(tickets.length).toEqual(1);
 	expect(tickets[0].price).toEqual(20);
 	expect(tickets[0].title).toEqual(title);
+});
+
+it('publishes an event', async () => {
+	const title = 'test publish';
+	await request(app)
+		.post('/api/tickets')
+		.set('Cookie', global.signin())
+		.send({
+			title,
+			price: 25,
+		})
+		.expect(201);
+
+	expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
