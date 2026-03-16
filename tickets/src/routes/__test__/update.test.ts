9b7e951f49939f1a646655af0ae2aa46a1012dd8
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2026-03-16 23:34:13 +0200

    test: add event publishing test for ticket updates using natsWrapper

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { it, expect } from '@jest/globals';
 import { Types } from 'mongoose';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('returns a 404 if the provided id does not exist', async () => {
 	const id = new Types.ObjectId().toHexString();
@@ -92,4 +93,27 @@ it('updates the ticket provided valid inputs', async () => {
 
 	expect(updatedTicketResponse.body.title).toEqual(updatedTitle);
 	expect(updatedTicketResponse.body.price).toEqual(updatedPrice);
+});
+
+it('publishes an event', async () => {
+	const cookie = global.signin();
+
+	const response = await request(app)
+		.post('/api/tickets')
+		.set('Cookie', cookie)
+		.send({ title: 'title', price: 24 })
+		.expect(201);
+
+	const updatedTitle = 'new title';
+	const updatedPrice = 50;
+	await request(app)
+		.put(`/api/tickets/${response.body.id}`)
+		.set('Cookie', cookie)
+		.send({
+			title: updatedTitle,
+			price: updatedPrice,
+		})
+		.expect(200);
+
+	expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
