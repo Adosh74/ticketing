462a2503ee9ceed09e431424e2dfc27e164fe1c3
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2026-05-04 00:19:56 +0300

    feat: enhance order retrieval with user authorization and error handling

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,21 @@
 import { Request, Response, Router } from 'express';
+import { Order } from '../models/order';
+import { NotAuthorizedError, NotFound, requireAuth } from '@mshebltickets/common';
 
 const router = Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-	res.send({});
+router.get('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+	const order = await Order.findById(req.params.orderId).populate('ticket');
+
+	if (!order) {
+		throw new NotFound();
+	}
+
+	if (order.userId !== req.currentUser!.id) {
+		throw new NotAuthorizedError();
+	}
+
+	res.send(order);
 });
 
 export { router as showOrderRouter };
