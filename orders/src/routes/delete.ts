d471ef8dba80f3edb0113a0fe674d454cda06be1
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2026-05-08 19:11:21 +0300

    feat: integrate order cancellation event publishing in delete route

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -6,6 +6,8 @@ import {
 	OrderStatus,
 	requireAuth,
 } from '@mshebltickets/common';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = Router();
 
@@ -14,7 +16,7 @@ router.delete(
 	requireAuth,
 	async (req: Request, res: Response) => {
 		const { orderId } = req.params;
-		const order = await Order.findById(orderId);
+		const order = await Order.findById(orderId).populate('ticket');
 
 		if (!order) {
 			throw new NotFound();
@@ -28,6 +30,12 @@ router.delete(
 		order.save();
 
 		// emit order cancelled event
+		new OrderCancelledPublisher(natsWrapper.client).publish({
+			id: order.id,
+			ticket: {
+				id: order.ticket.id,
+			},
+		});
 
 		res.status(204).send(order);
 	}
