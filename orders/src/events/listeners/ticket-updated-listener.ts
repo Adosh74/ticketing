6cc487876bc3ec3f93b2b9c24dfb20147ee6b7bb
Mohamed Shebl <98131964+Adosh74@users.noreply.github.com>
2026-05-15 19:47:12 +0300

    feat: implement TicketCreated and TicketUpdated listeners for event handling

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,26 @@
+import { Listener, Subjects, TicketUpdatedEvent } from '@mshebltickets/common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/ticket';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+	subject: Subjects.TickerUpdated = Subjects.TickerUpdated;
+
+	queueGroupName = queueGroupName;
+
+	async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+		const { price, title } = data;
+
+		const ticket = await Ticket.findById(data.id);
+
+		if (!ticket) {
+			throw new Error('Ticket not found');
+		}
+
+		ticket.set({ price, title });
+
+		await ticket.save();
+
+		msg.ack();
+	}
+}
